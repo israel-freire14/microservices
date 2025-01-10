356f67369ddabd6b3e846154179e50df00eaa2f8
Israel Freire <israelfreire999@gmail.com>
2025-01-09 22:20:51 -0300

    hr-worker feat: adding dependencies logger and enviroment

--- hr-worker/src/main/kotlin/com/freiredev/hrworker/controllers/AbstractController.kt
+++ hr-worker/src/main/kotlin/com/freiredev/hrworker/controllers/AbstractController.kt
@@ -1,12 +1,22 @@
 package com.freiredev.hrworker.controllers
 
 import com.freiredev.hrworker.services.WorkerService
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
 class AbstractController {
 
     @Autowired
+    lateinit var env: Environment
+
+    var logger: Logger = LoggerFactory.getLogger(AbstractController::class.java)
+
+    @Autowired
     lateinit var workerService: WorkerService
+
 }
