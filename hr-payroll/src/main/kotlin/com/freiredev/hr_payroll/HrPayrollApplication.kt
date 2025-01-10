9a748f94ad99e733b895e727b26c2c99a4f066c8
Israel Freire <israelfreire999@gmail.com>
2025-01-09 22:23:04 -0300

    hr-payroll feat: enable feign clients

--- hr-payroll/src/main/kotlin/com/freiredev/hr_payroll/HrPayrollApplication.kt
+++ hr-payroll/src/main/kotlin/com/freiredev/hr_payroll/HrPayrollApplication.kt
@@ -2,7 +2,11 @@ package com.freiredev.hr_payroll
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+
+
+@EnableFeignClients
 @SpringBootApplication
 class HrPayrollApplication
 
