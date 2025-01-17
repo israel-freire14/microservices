fe424a60374475a0b253e9297cb5175776acb477
Israel Freire <israelfreire999@gmail.com>
2025-01-17 11:29:31 -0300

    created module hr-api-gateway

---
+++ hr-api-gateway/src/main/kotlin/com/freiredev/hr_api_gateway/HrApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.freiredev.hr_api_gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrApiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrApiGatewayApplication>(*args)
+}
