aa0bef5b478815a632f1852c31e7fadcfb7d4f49
Israel Freire <israelfreire999@gmail.com>
2025-01-18 09:51:30 -0300

    feat: configuring api gateway

--- hr-api-gateway/build.gradle.kts
+++ hr-api-gateway/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
--- hr-api-gateway/src/main/kotlin/com/freiredev/hr_api_gateway/HrApiGatewayApplication.kt
+++ hr-api-gateway/src/main/kotlin/com/freiredev/hr_api_gateway/HrApiGatewayApplication.kt
@@ -2,7 +2,9 @@ package com.freiredev.hr_api_gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class HrApiGatewayApplication
 
