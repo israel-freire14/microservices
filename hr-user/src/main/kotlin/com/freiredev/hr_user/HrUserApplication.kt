1dc695e8c93a297c94d08e1047bc722ddf030e08
Israel Freire <israelfreire999@gmail.com>
2025-01-25 12:53:23 -0300

    hr-user | feat: module hr-user created

---
+++ hr-user/src/main/kotlin/com/freiredev/hr_user/HrUserApplication.kt
@@ -0,0 +1,11 @@
+package com.freiredev.hr_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrUserApplication>(*args)
+}
