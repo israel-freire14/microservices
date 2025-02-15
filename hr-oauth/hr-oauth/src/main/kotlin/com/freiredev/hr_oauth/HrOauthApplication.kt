6e9dae0620bfa78852b9fe75bbb1f118a7cc10a0
Israel Freire <130157902+israel-freire14@users.noreply.github.com>
2025-02-15 13:04:54 -0300

    hr-oauth | feat: hr-oauth module created

---
+++ hr-oauth/hr-oauth/src/main/kotlin/com/freiredev/hr_oauth/HrOauthApplication.kt
@@ -0,0 +1,11 @@
+package com.freiredev.hr_oauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrOauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrOauthApplication>(*args)
+}
